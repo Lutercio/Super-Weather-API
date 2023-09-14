be97b4623f0e9781739377c21380d3f6dfb57f3b
Lutercio Jackson <68786061+LuterKinggg@users.noreply.github.com>
2023-09-13 22:36:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 app = Flask(__name__)
 
 # Keys of acess of the base APIs 
-keys = ["", "", "", "", "", ""] # Put your keys here
+keys = ["AIzaSyDPGWdhfrPDMa2xMXUen940TptcccgUZrA", "b8e9cc118639cd4491d6aae15fd2b57e", "", "fbdb034be5e345d5b51184535232608", "3LFSFM734XHLRFZTTJ6SM638L", "eLXXd2jxYZTWjb1muJyOzUVNKCYYR0w7"]
 
 # Functions to "GET" requests of the base APIs
 def api0(lat, lng):
@@ -56,13 +56,13 @@ def api5(lat, lng):
 @app.route('/get/<city>')
 def get_weather(city):
     # Initializate some variables 
-    apicount = 0
+    apicountT = 0
+    apicountFL = 0
+    apicountWS = 0
     temperature = 0
     humidity = 0
     feels_like = 0
-    main = ''
-    description = ''
-
+    wind_speed = 0
 
     # Get the latitude and longitude of the city/place after the "get/" from google maps API
     location = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={city}&key={keys[0]}")
@@ -75,7 +75,7 @@ def get_weather(city):
 
     # Create the poll to manage the process
     with mp.Pool() as pool:
-        # Loop who get the request for each API
+        # Loop that get the request for each API
         for api_func in [api0, api1, api2, api3, api4, api5]:
             process = pool.apply_async(api_func, args=(lat, lng)) # Create the process to run any "api" funtion
             processes.append(process) # Append the function to the "processes" array
@@ -93,29 +93,64 @@ def get_weather(city):
             print("API ERROR")
     
     default_temp = weather_data[1]["current"]["temp"] # Use OpenWeather to compare with other APIs data
+    default_flike = weather_data[1]["current"]["feels_like"]
+    default_wspeed = weather_data[1]["current"]["wind_speed"]
     margin = 3 # Define a margin
 
     temp_data = [weather_data[0]["current_weather"]["temperature"], weather_data[1]["current"]["temp"], weather_data[2]["results"]["temp"], weather_data[3]["current"]["temp_c"], weather_data[4]["days"][0]["temp"], weather_data[5]["data"]["values"]["temperature"]]
+    flike_data = [weather_data[1]["current"]["feels_like"], weather_data[3]["current"]["feelslike_c"], weather_data[4]["days"][0]["feelslike"], weather_data[5]["data"]["values"]["temperatureApparent"]]
+    wspeed_data = [weather_data[0]["current_weather"]["windspeed"], weather_data[1]["current"]["wind_speed"], weather_data[3]["current"]["wind_kph"], weather_data[4]["days"][0]["windspeed"], weather_data[5]["data"]["values"]["windSpeed"]]
+
+    print("--------------------")
+    print("Temperature")
 
     i = 0
     while i < 6:
         if temp_data[i] > default_temp - margin and temp_data[i] < default_temp + margin:
             temperature += temp_data[i]
-            apicount += 1
+            apicountT += 1
             print(temp_data[i])
         else:
-            print(f"API{i} ERROR")
+            print(f"API{i} T ERROR")
         i += 1
+
+    print("--------------------")
+    print("Feels Like")
+
+    i = 0
+    while i < 4:
+        if flike_data[i] > default_flike - margin and flike_data[i] < default_flike + margin:
+            feels_like += flike_data[i]
+            apicountFL += 1
+            print(flike_data[i])
+        else:
+            print(f"API{i} FL ERROR")
+        i += 1
+
+    print("--------------------")
+    print("Wind Speed")
+    
+    i = 0
+    while i < 5:
+        if wspeed_data[i] > default_wspeed - margin and wspeed_data[i] < default_wspeed + margin:
+            wind_speed += wspeed_data[i]
+            apicountWS += 1
+            print(wspeed_data[i])
+        else:
+            print(f"API{i} WS ERROR")
+        i += 1
+
 
     # Data that will be returned
     final_api = {
-        'temperature': round(temperature / apicount, 2), # Do a arithmetic average of the sum of all temperature data and the number of API data collected
+        'temperature': round(temperature / apicountT, 2), # Do a arithmetic average of the sum of all temperature data and the number of API data collected
         'local_time': weather_data[3]["location"]["localtime"],
-        'feels_like': 0,
+        'feels_like': round(feels_like / apicountFL, 2),
         'humidity': 0,
         'clouds': 0,
-        'wind_speed': 0,
+        'wind_speed': round(wind_speed / apicountWS, 2),
         'weather':{
+            'id': weather_data[1]["current"]["weather"][0]["id"],
             'main': weather_data[1]["current"]["weather"][0]["main"],
             'description': weather_data[1]["current"]["weather"][0]["description"]
         }
